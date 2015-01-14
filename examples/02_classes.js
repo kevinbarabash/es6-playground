362f2754aba598afa9a565682bd1d09be191d101
Kevin Barabash <kevinb7@gmail.com>
2015-01-13 22:40:07 -0700

    initial implementation pushed to gh-pages

---
+++ examples/02_classes.js
@@ -0,0 +1,19 @@
+// source: https://6to5.org/docs/tour/#classes
+
+class SkinnedMesh extends THREE.Mesh {
+    constructor(geometry, materials) {
+        super(geometry, materials);
+
+        this.idMatrix = SkinnedMesh.defaultMatrix();
+        this.bones = [];
+        this.boneMatrices = [];
+        //...
+    }
+    update(camera) {
+        //...
+        super.update();
+    }
+    static defaultMatrix() {
+        return new THREE.Matrix4();
+    }
+}
